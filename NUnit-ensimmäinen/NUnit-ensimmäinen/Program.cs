336b47ffb7cb4b2c7f5314383cdd90a42c6ee25d
Jere Kauppinen <jere.kauppinen@student.hamk.fi>
2021-02-16 08:51:58 +0200

    added project

---
+++ NUnit-ensimmäinen/NUnit-ensimmäinen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NUnit_ensimmäinen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
