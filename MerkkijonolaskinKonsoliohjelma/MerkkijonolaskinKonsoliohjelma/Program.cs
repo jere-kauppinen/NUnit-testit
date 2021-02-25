018274cd00db5fb34f76aa1fbde18447ba8b82a0
Jere Kauppinen <jere.kauppinen@student.hamk.fi>
2021-02-25 12:16:27 +0200

    Lisätty Merkkijonolaskin

---
+++ MerkkijonolaskinKonsoliohjelma/MerkkijonolaskinKonsoliohjelma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MerkkijonolaskinKonsoliohjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
